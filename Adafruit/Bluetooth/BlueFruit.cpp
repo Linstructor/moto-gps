10e658f8707ec8d9834f1312dcfa8c93920e6dc8
Tristan M <tristan.morisot@viacesi.fr>
2021-07-03 16:35:14 +0200

    bluetooth module WIP

--- Adafruit/Bluetooth/BlueFruit.cpp
+++ Adafruit/Bluetooth/BlueFruit.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "BlueFruit.h"
+
+void BlueFruit::init() {
+    // setup spi
+
+    this->spi.deselectCs();
+
+    //Send init command
+    execute(SDEP_CMDTYPE_INITIALIZE, NULL, 0);
+
+    // reset chip
+
+    sleep_ms(1000);
+}
+
+void BlueFruit::execute(uint16_t command, uint8_t *buffer, uint8_t length) {
+    // Check if data is being transferred from the chip
+    // or if in data mode
+
+    /* Create header packet
+     * message type = command
+     * message id high = (input>>8) & 0xff
+     * message id low = input & 0xff
+     * length = length
+     * more data ?
+     */
+
+    if (buffer != NULL && length > 0) {
+        /* Copy buffer into message
+         * memcpy(msgCmd.payload, buf, count);
+         */
+    }
+
+    // Begin transaction
+
+    // Enable CS
+
+    //Wait device ack
+
+    // Create timeout
+
+    // While timeout is not reached or if the message has been confirmed ?
+    while ( ( spixfer(msgCmd.header.msg_type) == SPI_IGNORED_BYTE ) && !tt.expired() )
+    {
+        // Disable & Re-enable CS with a bit of delay for Bluefruit to ready itself
+        this->spi.deselectCs();
+        sleep_ms(10);
+        this->spi.selectCs();
+    }
+
+    // If not timeout, send the rest of the data
+    // spixfer((void*) (((uint8_t*)&msgCmd) +1), sizeof(sdepMsgHeader_t)+count-1);
+
+    this->spi.deselectCs();
+
+}
+
+void DataFIFO::flush() {
+    // Clear fifo
+}
--- Adafruit/Bluetooth/BlueFruit.h
+++ Adafruit/Bluetooth/BlueFruit.h
@@ -8,13 +8,34 @@
 
 #include "../BusDevice/SPI.h"
 
+const uint16_t SDEP_MAX_PACKETSIZE = 16;
+
+// Command list
+const uint16_t SDEP_CMDTYPE_INITIALIZE = 0xBEEF; /**< Controls the on board LED(s) */
+const uint16_t SDEP_CMDTYPE_AT_WRAPPER = 0x0A00;
+const uint16_t SDEP_CMDTYPE_BLE_UARTTX = 0x0A01;
+const uint16_t SDEP_CMDTYPE_BLE_UARTRX = 0x0A02;
+
+//Message type
+const uint16_t SDEP_MSGTYPE_COMMAND = 0x10;
+const uint16_t SDEP_MSGTYPE_RESPONSE = 0x20;
+const uint16_t SDEP_MSGTYPE_ALERT = 0x40;
+const uint16_t SDEP_MSGTYPE_ERROR = 0x80;
+
 class BlueFruit {
 public:
-    BlueFruit(SPI spi);
-    void reset();
+    BlueFruit(SPI spi, int rst, int irq);
 
 private:
     SPI spi;
+    void init();
+    void execute(uint16_t command, uint8_t *buffer, uint8_t length);
+    void read();
+};
+
+class DataFIFO {
+public:
+    void flush();
 };
 
 
